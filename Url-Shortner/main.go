93ca8b3126372a34df93d134983742ae1b8e11e8
Ayramuh-ynah <humayra.hany@gmail.com>
2026-06-11 16:23:44 +0600

    Url-Shortner

--- Url-Shortner/main.go
+++ Url-Shortner/main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"gorm.io/gorm"
@@ -58,6 +59,29 @@ func GenarateShortCode(n int) string {
 	return base64.URLEncoding.EncodeToString(b)[:n]
 }
 
+func CreateShortURL(c echo.Context) error {
+
+	type Request struct {
+		URL string `json:"url"`
+	}
+	req := new(Request)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid request"})
+	}
+	shortCode := GenarateShortCode(6)
+
+	url := models.URL{
+		OriginalURL: req.URL,
+		ShortCode:   shortCode,
+	}
+	
+	if err := db.Create(&url).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Failed to create short URL"})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{"short_url": fmt.Sprintf("%s/%s", baseURL, shortCode)})
+
+}
 func main() {
 	initDB()
 	e:= echo.New()
