b3c396bc79439b3fec2d12dd5111b9f1741f2eb8
Ayramuh-ynah <humayra.hany@gmail.com>
2026-06-23 11:21:44 +0600

    Complete

--- email-checker/main.go
+++ email-checker/main.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"strings"
 )
 
 
@@ -22,11 +24,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	return true
+	
 }
 
 func checkDomain(domain string) bool {
 
+	var hasMX, hasSPF, hasDMARC, hasDKIM, isValid bool
 
-	
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error looking up MX records for domain %s: %v", domain, err)
+	} 
+	if (len(mxRecords) > 0) {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error looking up TXT records for domain %s: %v", domain, err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error looking up DMARC records for domain %s: %v", domain, err)
+	} 
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%s, %t, %t, %s, %t, %s, %t, %t\n", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord, hasDKIM, isValid)
+
+	return hasMX && hasSPF && hasDMARC
+
 }
